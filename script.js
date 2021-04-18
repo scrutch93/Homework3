718fd69c690bc376d22300f7d95d646b1523cc2b
Steven Crutchfield <stevccru@icloud.com>
2021-04-17 22:36:37 -0400

    update

---
+++ script.js
@@ -0,0 +1,23 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+console.log(generateBtn)
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  if 
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword){
+  e.preventDefault()
+
+
+  
+}
+
+
