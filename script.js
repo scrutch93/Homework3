472ba444e6783a9272fc04aa84ed1d1511aea6b7
Steven Crutchfield <stevccru@icloud.com>
2021-04-18 15:11:19 -0400

    Updated

--- script.js
+++ script.js
@@ -1,13 +1,30 @@
 
-var optionUpper = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"]
-var optionLower = ["abcdefghijklmnopqrstuvwxyz"]
-var optionNumber = ['123456789']
-var optionSpec = ["#$%&)(*+,/:;<=>?@"]
-
-
+//This creates a promt for when the user clicks on the generate button. This also asks them how many characters they would like for their password to be.
 function promptCriteria (){
-  prompt("Please select the desired criteria for password");
+  var passwordLength = prompt("How many characters? ");
+  var errorFlag = false; //If the user puts in a value that does not meet the criteria, error message will display.
+
+  if (passwordLength < 8 && passwordLength > 128){
+    alert("Error, does not saisty requirements. Please ensure input value is greater than or equal to 8 and less than or equal to 128")
+    errorFlag = true;
+  }
+  // if it's inbetween 8 and 128 continu else do nothing
+  if (errorFlag === false) {
+
+  }
+  else {
+    
+  }
+
+
 }
+
+var optionUpper = ["A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z"]
+var optionLower = ["a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z"]
+var optionNumber = ['1,2,3,4,5,6,7,8,9']
+var optionSpec = ["#,$,%,&,),(,*,+,/,:,;,<,=,>,?,@,"]
+
+console.log(optionUpper)
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
