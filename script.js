0e65b3a94d851d08cf40c6870d13d3d78b3b081c
Steven Crutchfield <stevccru@icloud.com>
2021-04-18 11:23:29 -0400

    update

--- script.js
+++ script.js
@@ -2,27 +2,24 @@
 
 
 
-
-
 function promptCriteria (){
   prompt("Please select the desired criteria for password");
 }
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-console.log(generateBtn)
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
-  
+  passwordText.value = password;
+
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword)
-  Event.preventDefault()
-  const password = generatePassword (characterAmount, include)
+generateBtn.addEventListener("click", writePassword);
 
 
 
